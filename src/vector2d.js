2a6c5c709530e833346b6a8ef5e0cacdb42431e7
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-05-11 21:22:14 +0100

    add convertToLocal methods

--- src/vector2d.js
+++ src/vector2d.js
@@ -12,4 +12,28 @@
 		this.X = x;
 		this.Y = y;
 	}
+
+	Vector2D.prototype.length = function () {
+		return Math.sqrt(this.X * this.X + this.Y * this.Y);
+	};
+
+	Vector2D.prototype.distance = function (v) {
+		return this.sub(v).length();
+	};
+
+	Vector2D.prototype.angle = function () {
+		return Math.atan2(this.X, this.Y);
+	};
+
+	Vector2D.prototype.tryNormalize = function () {
+		let length = this.length();
+		return length === 0 ? Vector2D.zero() : this.div(length);
+	};
+
+	Vector2D.prototype.convertToLocal = function (dimensions) {
+		return new Vector2D(
+			this.X - dimensions.left,
+			this.Y - dimensions.top
+		).div(dimensions.scaleRatio);
+	};
 })();
