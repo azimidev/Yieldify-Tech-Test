34ef87a9d4e13af7feffded8629735edfc645de6
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-05-11 21:20:58 +0100

    create vector2d class

---
+++ src/vector2d.js
@@ -0,0 +1,15 @@
+/**
+ * Vector2D class.
+ * This file is taken from common GitHub Gist and modified.
+ * Some of the methods are only used for tests.
+ * I have written this class with ES6 first and then I compiled it to ES5
+ */
+
+(function () {
+	'use strict';
+
+	function Vector2D(x, y) {
+		this.X = x;
+		this.Y = y;
+	}
+})();
